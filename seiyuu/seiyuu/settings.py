8fcef202cd9dab54015bbd240afa2da6d81cb669
Akenaide <kevinms1@gmail.com>
2014-10-12 08:48:12 +0200

    init scrapy project

---
+++ seiyuu/seiyuu/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for seiyuu project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'seiyuu'
+
+SPIDER_MODULES = ['seiyuu.spiders']
+NEWSPIDER_MODULE = 'seiyuu.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'seiyuu (+http://www.yourdomain.com)'
