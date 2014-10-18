582a574eda49f7e46fb645fb7ffa8ef160aecd34
Akenaide <kevinms1@gmail.com>
2014-10-18 17:55:50 +0200

    add apps to settings

--- seiyuu/webfw/settings.py
+++ seiyuu/webfw/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
+    'webfw.seiyuu_mgr',
+
 )
 
 MIDDLEWARE_CLASSES = (
