189eb8641942debbe29a1de0f47ceb14284c7c74
Akenaide <kevinms1@gmail.com>
2014-10-18 18:57:25 +0200

    Initial models with migration

---
+++ seiyuu/webfw/seiyuu_mgr/migrations/0001_initial.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Anime',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('start_time', models.DateField()),
+                ('page_link', models.URLField()),
+                ('image_link', models.URLField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Character',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('season', models.CharField(max_length=200)),
+                ('status', models.CharField(max_length=200, choices=[(b'MAIN', b'main'), (b'SUPPORT', b'support')])),
+                ('page_link', models.URLField()),
+                ('image_link', models.URLField()),
+                ('last_modified', models.DateTimeField(auto_now=True, auto_now_add=True)),
+                ('anime', models.ForeignKey(to='seiyuu_mgr.Anime')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Seiyuu',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('first_name', models.CharField(max_length=30)),
+                ('last_name', models.CharField(max_length=30)),
+                ('page_link', models.URLField()),
+                ('image_link', models.URLField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='character',
+            name='seiyuu',
+            field=models.ForeignKey(to='seiyuu_mgr.Seiyuu'),
+            preserve_default=True,
+        ),
+    ]
--- seiyuu/webfw/seiyuu_mgr/models.py
+++ seiyuu/webfw/seiyuu_mgr/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Seiyuu(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    page_link = models.URLField()
+    image_link = models.URLField()
+
+class Anime(models.Model):
+    name = models.CharField(max_length=255)
+    start_time = models.DateField()
+    page_link = models.URLField()
+    image_link = models.URLField()
+
+class Character(models.Model):
+    STATUS = (
+        ("MAIN", "main"),
+        ("SUPPORT", "support")
+    )
+    seiyuu = models.ForeignKey('Seiyuu')
+    anime = models.ForeignKey('Anime')
+    season = models.CharField(max_length=200)
+    status = models.CharField(max_length=200, choices=STATUS)
+    page_link = models.URLField()
+    image_link = models.URLField()
+    last_modified = models.DateTimeField(auto_now=True, auto_now_add=True)
