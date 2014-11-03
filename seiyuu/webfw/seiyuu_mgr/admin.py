b3a51bb5c16013fe75f2dcb897d500f2da902a25
Akenaide <kevinms1@gmail.com>
2014-11-03 01:34:38 +0100

    seiyuu admin

--- seiyuu/webfw/seiyuu_mgr/admin.py
+++ seiyuu/webfw/seiyuu_mgr/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from webfw.seiyuu_mgr.models import Seiyuu
+from webfw.seiyuu_mgr.models import Anime
+from webfw.seiyuu_mgr.models import Character
+
+
+class SeiyuuAdmin(admin.ModelAdmin):
+    list_display = ("pk", "first_name", "last_name",)
+
+admin.site.register(Seiyuu, SeiyuuAdmin)
+
