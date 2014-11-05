1a59648045227eb6edbc543e5cea90ab3f1b9224
Akenaide <kevinms1@gmail.com>
2014-11-06 00:36:52 +0100

    new req, trying to avoid ban 1

--- seiyuu/crawl/tools.py
+++ seiyuu/crawl/tools.py
@@ -3,7 +3,13 @@ import os
 import requests
 import urlparse
 
+from dateutil import parser
+from faker import Factory
+
 GOOGLE_CACHE_URL = "http://webcache.googleusercontent.com/search?q=cache:%s"
+
+fake = Factory.create()
+session = requests.Session()
 def get_google_cache(url):
     """
     take a url and seek it in google cache
@@ -11,5 +17,8 @@ def get_google_cache(url):
     parsed = urlparse.urlparse(url)
     req_url = parsed.hostname + parsed.path
     google_cache = GOOGLE_CACHE_URL % req_url
+    session.headers.update({'user-agent':fake.user_agent()})
 
-    return requests.get(google_cache).url
+    return session.get(google_cache).url
+
+
