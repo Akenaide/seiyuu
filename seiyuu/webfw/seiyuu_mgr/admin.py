88153a37b3d04f3410346b61ac841e9c39051d76
Akenaide <kevinms1@gmail.com>
2014-11-08 19:18:46 +0100

    Extract Season

--- seiyuu/webfw/seiyuu_mgr/admin.py
+++ seiyuu/webfw/seiyuu_mgr/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from webfw.seiyuu_mgr.models import Seiyuu
 from webfw.seiyuu_mgr.models import Anime
 from webfw.seiyuu_mgr.models import Character
+from webfw.seiyuu_mgr.models import Season
 
 
 class SeiyuuAdmin(admin.ModelAdmin):
@@ -18,3 +19,8 @@ admin.site.register(Anime, AnimeAdmin)
 class CharacterAdmin(admin.ModelAdmin):
     list_display = ("pk", "first_name",)
 admin.site.register(Character, CharacterAdmin)
+
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ("label",)
+
+admin.site.register(Season, SeasonAdmin)
--- seiyuu/webfw/seiyuu_mgr/models.py
+++ seiyuu/webfw/seiyuu_mgr/models.py
@@ -16,7 +16,7 @@ class Anime(models.Model):
     start_time = models.DateField()
     page_link = models.URLField(null=True)
     image_link = models.URLField(null=True)
-    season = models.CharField(max_length=200)
+    season = models.ForeignKey('Season', null=True)
 
     def __unicode__(self):
         return u"%s" % self.name
@@ -37,3 +37,9 @@ class Character(models.Model):
 
     def __unicode__(self):
         return u"%s %s" % (self.first_name, self.last_name)
+
+class Season(models.Model):
+    label = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return u"%s" % self.label
