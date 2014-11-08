aa531bdc88fe3df889e91e0be9ba01f76a7a7134
Akenaide <kevinms1@gmail.com>
2014-11-08 17:55:03 +0100

    First useless view

---
+++ seiyuu/webfw/seiyuu_mgr/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+
+from webfw.seiyuu_mgr import views
+
+urlpatterns = patterns('webfw.seiyuu_mgr.views',
+    # Examples:
+    # url(r'^$', 'webfw.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', "seiyuu.seiyuu_list", name="seiyuu_list"),
+)
---
+++ seiyuu/webfw/seiyuu_mgr/views/seiyuu.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+def seiyuu_list(request):
+    """
+    """
+    return HttpResponse("<html>yay</html>")
