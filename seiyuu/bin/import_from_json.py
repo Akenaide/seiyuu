ceaad63c3b9b7fa662649df7bc21fb9751f3ebda
Akenaide <kevinms1@gmail.com>
2014-11-12 23:55:48 +0100

    ugly importer part2

--- seiyuu/bin/import_from_json.py
+++ seiyuu/bin/import_from_json.py
@@ -3,8 +3,10 @@
 import os
 import json
 os.environ['DJANGO_SETTINGS_MODULE'] = 'webfw.settings'
-
+import sys
 from scrapy.exceptions import DropItem
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(PROJECT_ROOT)
 
 from crawl import items
 from crawl import pipelines
@@ -14,34 +16,43 @@ def main():
     """
     json_data = json.load(open("items_seiyuu_5.json", "r"))
 
-    scrapy_items = list()
+    character_scrapy_items = list()
+    seiyuu_scrapy_items = list()
+    anime_scrapy_items = list()
 
     for entry in json_data:
         _type = entry.pop("_type")
 
-        if _type == "AnimeItems":
-            _anime = items.AnimeItems()
-            _anime.update(entry)
-            scrapy_items.append(_anime)
+        if _type == "SeiyuuItem":
+            _seiyuu = items.SeiyuuItem()
+            _seiyuu.update(entry)
+            seiyuu_scrapy_items.append(_seiyuu)
 
-        if _type == "AnimeItems":
-            _anime = items.AnimeItems()
+        if _type == "AnimeItem":
+            _anime = items.AnimeItem()
             _anime.update(entry)
-            scrapy_items.append(_anime)
+            anime_scrapy_items.append(_anime)
 
-        if _type == "AnimeItems":
-            _anime = items.AnimeItems()
-            _anime.update(entry)
-            scrapy_items.append(_anime)
+        if _type == "CharacterItem":
+            _character = items.CharacterItem()
+            _character.update(entry)
+            character_scrapy_items.append(_character)
 
     new_scrapy_items = list()
 
     duplicate = pipelines.DuplicatesPipeline()
-    for items in scrapy_items:
-        try:
-            new_scrapy_items.append(duplicate.process_item(items, ""))
-        except DropItem:
-            pass
+    create_item = pipelines.SeiyuuPipeline()
+    all_items = [anime_scrapy_items, seiyuu_scrapy_items, character_scrapy_items]
+    for sub_list in all_items:
+        for item in sub_list:
+            try:
+                new_item = duplicate.process_item(item, "")
+                create_item.process_item(new_item, "")
+            except DropItem:
+                print("Ignore %r" % item)
+                pass
+
+    # print("Create : %r" % new_scrapy_items)
 
 if __name__ == "__main__":
     main()
