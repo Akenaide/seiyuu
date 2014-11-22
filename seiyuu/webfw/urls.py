33751f89fd8e0fcdbff129ec714d169d2b7b972a
Akenaide <kevinms1@gmail.com>
2014-11-22 10:08:05 +0100

    Add home redirect

--- seiyuu/webfw/urls.py
+++ seiyuu/webfw/urls.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
 
@@ -10,5 +11,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^seiyuu/', include("webfw.seiyuu_mgr.urls", "seiyuu_mgr", "seiyuu_mgr"))
+    url(r'^seiyuu/', include("webfw.seiyuu_mgr.urls", "seiyuu_mgr", "seiyuu_mgr")),
+    url(r'^$', RedirectView.as_view(url="/seiyuu/",), name="home"),
 )
