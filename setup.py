26164fe19dad6f3125787dffc4ce3019c5f08d63
Kevin MS <kevin.mansang@mhcomm.fr>
2018-12-13 11:21:36 +0100

    upgrade requests version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires = [
         "django==1.6.8",
         "fake-factory==0.4.2",
-        "requests==2.4.3",
+        "requests",
         "python-dateutils==2.2"
         ]
 )
